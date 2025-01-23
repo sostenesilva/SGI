0f1f9b1c046455bcb98286bc6fc7dc26013a413c
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-01-23 15:34:44 -0300

    Atualização estética

--- HOME/views.py
+++ HOME/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Modulo
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def portal_inicial(request):
     modulos = Modulo.objects.all()
     return render(request, 'portal_inicial.html', {'modulos': modulos})
 
+login_required
 def modulo_detalhes(request, modulo_id):
     modulo = get_object_or_404(Modulo, pk=modulo_id)
     return render(request, 'modulo_detalhes.html', {'modulo': modulo})
