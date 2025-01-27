5e012a77622045364f1be2b0d106e399199e2cb1
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-01-27 14:59:13 -0300

    Atualização Secretarias

--- HOME/models.py
+++ HOME/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Secretaria(models.Model):
     nome = models.TextField()
 
+    def __str__(self):
+        return self.nome
+
 class Modulo(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
