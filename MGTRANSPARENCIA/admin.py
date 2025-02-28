8c259a6095d2ecb527f97d61fe278b0841d47545
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-02-27 23:58:45 -0300

    Ajustes

--- MGTRANSPARENCIA/admin.py
+++ MGTRANSPARENCIA/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.DbDimensao)
-admin.site.register(models.DbCriterios)
 admin.site.register(models.DbAvaliacao)
 admin.site.register(models.DbAvaliacaoLog)
+
+
+@admin.register(models.DbCriterios)
+class DbCriterios(admin.ModelAdmin):
+    # list_display = ('NumeroContrato','AnoContrato','TipoProcesso','NumeroProcesso','AnoProcesso','Fornecedor')
+    list_editable = ('dimensao','periodicidade')
+    # list_filter = ('AnoContrato', 'UnidadeGestora')
