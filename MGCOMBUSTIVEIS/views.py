808552bde19b0c7782c64829243d14757d37723d
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-03-10 15:30:31 -0300

    total abastecimentos

--- MGCOMBUSTIVEIS/views.py
+++ MGCOMBUSTIVEIS/views.py
@@ -67,9 +67,6 @@ def add_abastecimento(request):
         print(form.is_valid())
 
         if form.is_valid():
-            # print(form.cleaned_data)
-            # form.instance.valorTotal = form.instance.valorUnitario * form.instance.quantidade
-            # print(form.instance)
             form.save()
             return HttpResponse(
                 status=204,
