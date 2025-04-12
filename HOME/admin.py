6ec2542856253ed2aa8bb9c15469e662c622374b
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-04-12 10:16:56 -0300

    Admin Registers

--- HOME/admin.py
+++ HOME/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Modulo)
-admin.site.register(models.Tutorial)
-admin.site.register(models.Secretaria)
-admin.site.register(models.Setor)
+@admin.register(models.Secretaria)
+class Secretaria(admin.ModelAdmin):
+    list_display = ('nome','sigla','ativo')
+    list_editable = ('ativo',)
+
+@admin.register(models.Setor)
+class Setor(admin.ModelAdmin):
+    list_display = ('nome','sigla', 'secretaria','ativo')
+    list_editable = ('ativo',)
+
+@admin.register(models.Modulo)
+class Modulo(admin.ModelAdmin):
+    list_display = ('nome', 'permissao')
+
+@admin.register(models.Tutorial)
+class Tutorial(admin.ModelAdmin):
+    list_display = ('titulo','modulo')
+
+@admin.register(models.Notificacao)
+class Notificacao(admin.ModelAdmin):
+    list_display = ('titulo', 'usuario', 'lida', 'criada_em')
--- MGPROTOCOLO/admin.py
+++ MGPROTOCOLO/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Documento)
-admin.site.register(models.Movimentacao)
+
+@admin.register(models.Movimentacao)
+class Movimentacao(admin.ModelAdmin):
+    list_display = ('processo', 'realizado_por', 'realizado_em', 'setor_destino','confirmacao')
+
 admin.site.register(models.Setor)
-admin.site.register(models.Processo)
+
+@admin.register(models.Processo)
+class Processo(admin.ModelAdmin):
+    list_display = ('numero','descricao','setor_atual')
+    list_editable = ('descricao',)
+
 admin.site.register(models.ProtocoloMovimentacao)
