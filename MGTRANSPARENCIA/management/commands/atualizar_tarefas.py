fa5d893f57ef790e3e0cd0119dc5499a9e3767e9
Sóstenes Melo <sostenesilvaa@gmail.com>
2025-04-30 18:53:35 -0300

    correção do cron

--- MGTRANSPARENCIA/management/commands/atualizar_tarefas.py
+++ MGTRANSPARENCIA/management/commands/atualizar_tarefas.py
@@ -8,11 +8,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         ## A data limite da tarefa tem hora 00:00, por isso o deslocamento de 1 dia na data atual.
-        agora = timezone.now() - timedelta(days=1)
+        agora = timezone.now()
 
         tarefas_atrasadas = DbAvaliacaoLog.objects.filter(
             status='Pendente',
-            data_limite__lt=agora
+            data_limite__lt=agora - timedelta(days=1)
         )
         count = tarefas_atrasadas.count()
         tarefas_atrasadas.update(status='Atrasado')
